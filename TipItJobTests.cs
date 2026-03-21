811fa2be45d3a77678d99e22956438e24126e89b
Steve Colonna <quarterback16@live.com.au>
2026-03-21 13:02:15 +1000

    Added ability to test tipping job

--- TipItJobTests.cs
+++ TipItJobTests.cs
@@ -4,13 +4,29 @@
     public class TipItJobTests
     {
         [TestMethod]
+        public void ProgramCanRunTippingJobs()
+        {
+            var result = Program.TipitJob(
+                new ButlerCoreContext
+                {
+                    TippingSeason = DateTime.Now.Year,
+                    Logger = new NullLogger(),
+                    DropBoxFolder = "d:/Dropbox/",
+                    ResultsFile = "d:/Dropbox/json/results.json",
+                    StartDateTime = DateTime.Now,
+                });
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
         public void CanCalculateNewTippingState()
         {
             var ts = new TipItService.TipItService(
                 "d://dropbox//");
 
             var newState = ts.GetNewTippingState(
-                new DateTime(2024,6,28,0,0,0,DateTimeKind.Unspecified));
+                new DateTime(2026,3,21,0,0,0,DateTimeKind.Unspecified));
             Assert.IsNotNull(newState);
             Console.WriteLine( $"{ts.NewResults.Count} new results added");
             ts.NewResults.ForEach(
